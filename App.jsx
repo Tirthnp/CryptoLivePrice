801d857655b2bce0405eb6b430a5a9886282a0ab
Soham Tomar <soham.tomargilly@gmail.com>
2020-10-23 23:31:59 -0230

    Added visual cues for price updates

--- App.jsx
+++ App.jsx
@@ -7,7 +7,16 @@ const e = React.createElement;
 
 function App(props) {
   return (
-    <div style = {{ display: 'flex', flexDirection: 'column', justifyContent:"space-evenly", height:'100vh', backgroundColor:'black'}}>
+    <div style = {{ display: 'flex', 
+    flexDirection: 'column', 
+    justifyContent:"space-evenly", 
+    height:'max-content', 
+    backgroundColor:'black',
+    width: 'max-content',
+    margin: 'auto',
+    marginTop: '75px',
+    marginBottom: '75px'
+    }}>
       <Price symbol="BTCUSDT" name="Bitcoin"/>
       <Price symbol="ETHUSDT" name="Ethereum"/>
       <Price symbol="DOTUSDT" name="PolkaDot"/>
--- Price.jsx
+++ Price.jsx
@@ -3,8 +3,11 @@
 
 function Price(props){
     const baseUrl = 'https://api.binance.com/api/v3/ticker/price?symbol='+props.symbol;
-    const [price, setPrice] = React.useState("Loding...");
+    const [price, setPrice] = React.useState("Loading...");
     const [temp, setTemp] = React.useState(false);
+    const [prevPrice, setPrevPrice] = React.useState(-9999999);
+
+
     let req = new XMLHttpRequest();
     React.useEffect(() => {
         setTimeout(() => {
@@ -12,25 +15,37 @@ function Price(props){
             req.open('GET',baseUrl,true);
             req.send();
             req.onload = (() => {
-                setPrice('$'+JSON.parse(req.responseText).price);
-                
+                setPrevPrice(price);
+                setPrice(Number(JSON.parse(req.responseText).price));
+
             });
         },500);
         
     }, [temp]);
+
+
+    const setColor = (() => {
+        if(!(typeof(price) === 'string')){
+            if(price >= prevPrice) {
+                return '#7CFC00'
+            }
+            else return '#FF0000'
+        }
+        else return 'white'
+    })
+
     let style = { 
         backgroundColor:"black", 
-        color:"white", 
+        color:setColor(), 
         fontSize:"5em", 
         display:"flex",
         flexDirection:"row",
         justifyContent:"space-evenly"
     }
-
     return(
         <div style={style}>
-            <div>{props.name}</div>
-            <div>{price}</div>
+            <div style = {{color: 'white'}}>{props.name}</div>
+            <div>{"$ " + price}</div>
         </div>
     )
 }
